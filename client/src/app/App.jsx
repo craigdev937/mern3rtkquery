b5f4180d5a1e5e0af53c3440542eff2a6335224c
Craig <indigomx9@gmail.com>
2022-08-21 21:30:54 -0400

    Initial setup of the project.

---
+++ client/src/app/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis laudantium, necessitatibus maiores officia fuga corrupti autem ipsam nihil illo praesentium sit alias voluptatibus possimus. Maxime cumque sequi vel reprehenderit sint.</p>
+        </React.Fragment>
+    );
+};
+
+
